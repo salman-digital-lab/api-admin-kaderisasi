c2e590be3a9e02b6b417d51b311ae392196cec49
unknown <wtesting231@gmail.com>
2023-01-23 10:57:52 +0800

    export data student care

--- start/routes/StudentCareRoutes.js
+++ start/routes/StudentCareRoutes.js
@@ -1,9 +1,12 @@
+
 const Route = use('Route')
 
 Route.group(() => {
   Route.delete(':id', 'StudentCareController.delete')
+  Route.get('/export', 'StudentCareController.export')
   Route.get('', 'StudentCareController.get')
   Route.get('counselors', 'StudentCareController.getCounselors')
   Route.get(':id', 'StudentCareController.getById')
   Route.put(':id', 'StudentCareController.update')
-}).prefix('/v1/student-care').middleware(['auth', 'activeUser', 'privileges:student-care'])
+}).prefix('/v1/student-care').middleware(['auth', 'activeUser', 'privileges:student-care']);
+
