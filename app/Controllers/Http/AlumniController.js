c89f1d46822a3decede1887916b8b868f1bf0986
unknown <admin@salmanitb.com>
2023-09-10 06:39:52 +0700

    feat: add store, show, update, delete function and register routes

--- app/Controllers/Http/AlumniController.js
+++ app/Controllers/Http/AlumniController.js
@@ -52,18 +52,175 @@ class AlumniController {
 
     async show({params, response}) {
 
+        const { id } = params;
+
+        try {
+
+            let alumni = await Alumni.findOrFail(id)
+
+            return response.status(200).json({
+                status: "SUCCESS",
+                message: "Data Alumni berhasil dimuat!",
+                data: alumni,
+            })
+
+        } catch (error) {
+
+            return response.status(500).json({
+                status: "FAILED",
+                message: error.message,
+            })
+
+        }
+
     }
 
     async store({request, response}) {
 
+        const rules = {
+            name: "required",
+            email: "required"
+        };
+      
+        const validation = await validate(request.all(), rules);
+    
+        if (validation.fails()) {
+
+            return response.status(400).json({
+                status: "FAILED",
+                message: validation.messages(),
+            })
+        }
+    
+        try {
+
+            const data = request.all()
+
+            const checkIfExist = await Alumni.findBy('email', data.email)
+
+            if (checkIfExist != null) {
+                return response.status(400).json({
+                    status: "FAILED",
+                    message: "Email Alumni sudah terdaftar di dalam sistem",
+                })
+            }
+
+            let alumni = new Alumni();
+            alumni.name = data.name;
+            alumni.email = data.email;
+            alumni.whatsapp_number = (data.whatsapp_number != null) ? data.whatsapp_number : null ;
+            alumni.full_address = data.full_address;
+            alumni.occupation = data.occupation;
+            alumni.current_instance = data.current_instance;
+            alumni.bachelor_degree = data.bachelor_degree;
+            alumni.master_degree = data.master_degree;
+            alumni.doctoral_degree = data.doctoral_degree;
+            alumni.contributions = data.contributions;
+            alumni.is_donor = data.is_donor;
+            alumni.is_subscriber = data.is_subscriber;
+            alumni.notes = data.notes;
+            await alumni.save();
+        
+            alumni = await Alumni.findOrFail(alumni.id);
+        
+            return response.status(201).json({
+                status: "SUCCESS",
+                message: "Data Alumni berhasil dibuat!",
+                data: alumni,
+            });
+
+        } catch (error) {
+
+            return response.status(500).json({
+                status: "FAILED",
+                message: error.message,
+            });
+        }
     }
 
     async update({request, response, params}) {
 
+        const rules = {
+            name: "required",
+            email: "required"
+        };
+      
+        const validation = await validate(request.all(), rules);
+    
+        if (validation.fails()) {
+
+            return response.status(400).json({
+                status: "FAILED",
+                message: validation.messages(),
+            })
+        }
+
+        const { id } = params;
+
+        try {
+            const data = request.all()
+            const alumni = await Alumni.findByOrFail("id", id);
+
+            Object.keys(data).forEach((column) => {
+                alumni.merge({
+                [column]: data[column],
+                });
+            });
+
+            await alumni.save();
+
+            alumni = await Alumni.find(id);
+
+            return response.status(200).json({
+                status: "SUCCESS",
+                message: "Data Alumni berhasil diperbarui!",
+                data: alumni,
+            });
+
+        } catch (error) {
+
+            if (error.name === "ModelNotFoundException") {
+                return response.status(404).json({
+                status: "FAILED",
+                message: "Tidak ada data yang ditemukan",
+                });
+            }
+
+            return response.status(500).json({
+                status: "FAILED",
+                message: error.message,
+            });
+        }
     }
 
     async delete({params, response}) {
 
+        const { id } = params
+
+        try {
+
+            const alumni = await Alumni.findOrFail(id)
+            await alumni.delete()
+
+            return response.status(200).json({
+                status: "SUCCESS",
+                message: "Data Alumni berhasil dihapus!",
+                data: alumni,
+            })
+
+        } catch (error) {
+            if (error.name === "ModelNotFoundException") {
+                return response.status(404).json({
+                status: "FAILED",
+                message: "Data yang ingin dihapus tidak ditemukan",
+                });
+            }
+
+            return response.status(500).json({
+                status: "FAILED",
+                message: error.message,
+            });
+        }
     }
 }
 
---
+++ start/routes/AlumniRoutes.js
@@ -0,0 +1,11 @@
+const Route = use("Route");
+
+Route.group(() => {
+  Route.get("", "AlumniController.index");
+  Route.get("/:id", "AlumniController.show");
+  Route.post("", "AlumniController.store");
+  Route.put("/:id", "AlumniController.update");
+  Route.delete("/:id", "AlumniController.delete");
+})
+  .prefix("/v1/alumni")
+  .middleware(["auth", "activeUser", "privileges:member"]);
